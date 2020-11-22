6a26ddb82dea233c4fc92735ecad5fef920800da
dernoxen <bau.darah.manusia@gmail.com>
2020-11-22 22:17:03 +0700

    2

---
+++ arm/ansi.py
@@ -0,0 +1,8 @@
+class Text:
+    red     = "\033[2;31m"
+    green   = "\033[2;32m"
+    yellow  = "\033[2;33m"
+    blue    = "\033[2;34m"
+    purple  = "\033[2;35m"
+    cyan    = "\033[2;36m"
+    normal  = "\033[2;37m"
